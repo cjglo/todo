caffb995365dd114a01ca3fdab80abafd1cbfedc
cjglo <chris.j.gallo3@gmail.com>
2024-03-04 16:56:56 -0600

    refactor and added directory to file path

--- src/todo_handler.rs
+++ src/todo_handler.rs
@@ -25,11 +25,13 @@ pub struct ToDoHandler {}
 
 // TODO currently doesn't need to be struct, but may leave because fields may be required in future
 impl ToDoHandler {
+
+    const TODO_DIRECTORY: &'static str = "/todo-program-info";
     const TODO_FILE_NAME: &'static str = "/todo_data.ron";
 
     const LINE_LENGTH_LIMIT: u16 = 60;
     const NO_DUE_DATE_TEXT: &'static str  = "<no due date>";
-    const SEPERATOR_BETWEEN_TASK_AND_DATE: &'static str = "  |  ";
+    const PADDING_BETWEEN_TASK_AND_DATE: &'static str = "  |  ";
     const TITLE_BEFORE_DUE_DATE: &'static str = "DUE ";
     const DEFAULT_MARKET_CHAR: char = '⌛';
 
@@ -40,17 +42,20 @@ impl ToDoHandler {
         current_path: PathBuf,
     ) -> io::Result<()> {
         let path =
-            current_path.parent().unwrap().to_str().unwrap().to_string() + Self::TODO_FILE_NAME;
+            current_path.parent().unwrap().to_str().unwrap().to_string() + Self::TODO_DIRECTORY;
 
-        if !fs::metadata(&path).is_ok() {
-            fs::write(path.clone(), "[]".to_string())?;
+        let directory_path = std::path::Path::new(&path);
+        if !directory_path.is_dir() {
+            fs::create_dir_all(directory_path)?;
+        }
+        let file_path = path + Self::TODO_FILE_NAME;
+        if !fs::metadata(&file_path).is_ok() {
+            fs::write(file_path.clone(), "[]".to_string())?;
             println!("{}", "Created new RON file for To-Do Data.".bright_yellow());
         }
 
-        let file = File::open(path.clone())?;
+        let file = File::open(file_path.clone())?;
         let reader = BufReader::new(file);
-
-        // Read and deserialize the RON data
         let mut todos: Vec<ToDo> = ron::de::from_reader(reader).unwrap();
 
         if args_handler.help_flag {
@@ -66,7 +71,7 @@ impl ToDoHandler {
             print!("You Completed:");
             print!("\n{}", removed.task.bright_purple());
             println!("  ✔️");
-            let mut file = File::create(path)?;
+            let mut file = File::create(file_path)?;
             file.write_all(ron::ser::to_string(&todos).unwrap().as_bytes())?;
         } else if args_handler.marker_flag_and_index.is_some() {
             let to_edit = &mut todos[args[args_handler.marker_flag_and_index.unwrap() + 1].parse::<usize>().unwrap()];
@@ -79,14 +84,14 @@ impl ToDoHandler {
                     None => Some(Self::DEFAULT_MARKET_CHAR)
                 }
             }
-            let mut file = File::create(path)?;
+            let mut file = File::create(file_path)?;
             file.write_all(ron::ser::to_string(&todos).unwrap().as_bytes())?;
         }  else if args_handler.change_flag_and_index.is_some() {
             let to_edit = &mut todos[args[args_handler.change_flag_and_index.unwrap() + 1].parse::<usize>().unwrap()];
             let new_due_date = args.get(args_handler.change_flag_and_index.unwrap() + 2);
             to_edit.due_date = if new_due_date.is_some() { Some(new_due_date.unwrap().clone().to_uppercase()) } else { None };
             todos.sort_by(Self::todo_compare);
-            let mut file = File::create(path)?;
+            let mut file = File::create(file_path)?;
             file.write_all(ron::ser::to_string(&todos).unwrap().as_bytes())?;
         } else {
             // add items to to-do
@@ -96,7 +101,7 @@ impl ToDoHandler {
             }
             todos.push(to_add);
             todos.sort_by(Self::todo_compare);
-            let mut file = File::create(path)?;
+            let mut file = File::create(file_path)?;
             file.write_all(ron::ser::to_string(&todos).unwrap().as_bytes())?;
         }
 
@@ -122,37 +127,28 @@ impl ToDoHandler {
             if i != 0 {
                 println!();
             }
+            // This will check if to-do should be marked as dim, then color the number of the line, and break it it up to format it correctly
             if todo.char_marker.is_some() {
                 print!("{0: <63}", task_lines[0].bright_black());
                 if let Some(date) = &todo.due_date {
-                    println!("{}{}{}{}{}", Self::SEPERATOR_BETWEEN_TASK_AND_DATE.bright_black(), Self::TITLE_BEFORE_DUE_DATE.bright_black(), date.bright_black(), Self::SEPERATOR_BETWEEN_TASK_AND_DATE.bright_black(), todo.char_marker.unwrap());
+                    println!("{}{}", (Self::PADDING_BETWEEN_TASK_AND_DATE.to_string() + Self::TITLE_BEFORE_DUE_DATE + date + Self::PADDING_BETWEEN_TASK_AND_DATE).bright_black(), todo.char_marker.unwrap());
                 }
                 else {
-                    println!("{}{}{}{}", Self::SEPERATOR_BETWEEN_TASK_AND_DATE, Self::NO_DUE_DATE_TEXT.bright_black(), Self::SEPERATOR_BETWEEN_TASK_AND_DATE.bright_black(), todo.char_marker.unwrap());
-                }
-                if task_lines.len() > 1 {
-                    task_lines
-                        .iter()
-                        .skip(1)
-                        .for_each(|x| println!("{0: <50}", x.bright_black()));
+                    println!("{}{}", (Self::PADDING_BETWEEN_TASK_AND_DATE.to_string() + Self::NO_DUE_DATE_TEXT + Self::PADDING_BETWEEN_TASK_AND_DATE).bright_black(), todo.char_marker.unwrap());
                 }
             }
             else {
                 print!("{0}{1: <60}", task_lines[0].get(0..header_length).unwrap().bright_yellow(), task_lines[0].chars().skip(header_length).collect::<String>().bright_blue());
                 if let Some(date) = &todo.due_date {
                     let colored_date = if HIGHLIGHTED_DUE_DATES.contains(date) { date.bright_red() } else { date.bright_yellow() };
-                    println!("{}{}{}", Self::SEPERATOR_BETWEEN_TASK_AND_DATE, Self::TITLE_BEFORE_DUE_DATE, colored_date);
+                    println!("{}{}{}", Self::PADDING_BETWEEN_TASK_AND_DATE, Self::TITLE_BEFORE_DUE_DATE, colored_date);
                 }
                 else {
-                    println!("{}{}", Self::SEPERATOR_BETWEEN_TASK_AND_DATE, Self::NO_DUE_DATE_TEXT.green().dimmed());
-                }
-                if task_lines.len() > 1 {
-                    task_lines
-                        .iter()
-                        .skip(1)
-                        .for_each(|x| println!("{0: <50}", x.bright_blue()));
+                    println!("{}{}", Self::PADDING_BETWEEN_TASK_AND_DATE, Self::NO_DUE_DATE_TEXT.green().dimmed());
                 }
             }
+
+            Self::print_remaining_task_lines(task_lines, todo.char_marker.is_some());
         }
 
         println!("{}", line.bright_white());
@@ -201,6 +197,16 @@ Examples:
                 }
 
             }
+        }
+    }
+
+    fn print_remaining_task_lines(task_lines: Vec<&str>, is_marked: bool) {
+        let color_lambda = |x: &str| if is_marked {x.bright_black()} else {x.bright_blue()};
+        if task_lines.len() > 1 {
+            task_lines
+                .iter()
+                .skip(1)
+                .for_each(|x| println!("{0: <50}", color_lambda(x)));
         }
     }
 }
